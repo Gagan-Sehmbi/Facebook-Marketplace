e6bbdef05117ab8a1ea2335256defa8dbe29f1ce
Gagan Sehmbi <gagan.sehmbi@outlook.com>
2022-04-19 21:14:29 +0100

    Initial data exploration of missing values

--- clean_tabular.py
+++ clean_tabular.py
@@ -1,12 +1,18 @@
-# %%
+# %% 
+# IMPORT LIBRARIES
+from os import link
 import numpy as np
 import pandas as pd
 
 import pymysql
 from sqlalchemy import create_engine, inspect
 
+import missingno as msno
 
-# %%
+print('Done')
+
+# %% 
+# IMPORT TABULAR DATA
 DATABASE_TYPE= 'postgresql'
 DBAPI= 'psycopg2' 
 ENDPOINT= 'products.c8k7he1p0ynz.us-east-1.rds.amazonaws.com'
@@ -21,6 +27,21 @@ engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{DBUSER}:" f"{DBPASSWORD}@{EN
 engine.connect()
 
 df = pd.read_sql_table( 'products', engine, columns=["id", "product_name", "category", "product_description", "price", "location", "page_id", "create_time"]) 
-
+#df.replace('N/A', np.nan, inplace=True)
 df.to_csv('tabular_data.csv')
+
 # %%
+df = pd.read_csv(
+    'tabular_data.csv',
+    lineterminator='\n',
+    index_col=0)
+
+print('Done')
+df
+# %%
+# EXPLORE TABULAR DATA
+print(df.info())
+msno.matrix(df)
+
+print('Done')
+
