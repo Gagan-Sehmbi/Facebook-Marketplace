815d7795962824c5b413ed5757b5c7c060f8b7c7
Gagan Sehmbi <gagan.sehmbi@outlook.com>
2022-05-24 21:12:28 +0100

    Move image files to train/ test --> class folders

--- split_data.py
+++ split_data.py
@@ -1,41 +1,48 @@
 # %%
 # IMPORT LIBRARIES
 
+from random import shuffle
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import os
 
 
 # %%
 # IMPORT TABULAR DATA
 
 df = pd.read_csv('tabular_data.csv', index_col=0).drop(columns=['index'])
-df
+
+# %%
+# CREATE DIRECTORIES
+
+labels = df.loc[:, 'Category'].unique()
+
+for t in ['train', 'test']:
+    parent_dir = '/home/gagansehmbi/Documents/AiCore/Facebook-Marketplace/images'
+    os.mkdir(os.path.join(parent_dir, t))
+    for label in labels:
+        os.mkdir(os.path.join(parent_dir, t, label))
+
+
 # %%
 # SPLIT TABULAR DATA
 
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
-df_train, df_val = train_test_split(df_train, test_size=0.25, random_state=42)
-
+df_train, df_test = train_test_split(df, test_size=0.2, shuffle=True, random_state=42)
 
 df_train.to_csv('tabular_data_train.csv')
-df_val.to_csv('tabular_data_val.csv')
 df_test.to_csv('tabular_data_test.csv')
-
-train_images = df_train.loc[:, 'id'].tolist()
-val_images = df_val.loc[:, 'id'].tolist()
-test_images = df_test.loc[:, 'id'].tolist()
-
 
 # %%
 # SPLIT IMAGE DATA
 
-for image in train_images:
-    os.replace(f'images/{image}.jpg', f'images/train/{image}.jpg')
+for idx in df_train.index:
+    id = df_train.loc[idx, 'id']
+    label = df_train.loc[idx, 'Category']
+    os.replace(f'images/{id}.jpg', f'images/train/{label}/{id}.jpg')
 
-for image in val_images:
-    os.replace(f'images/{image}.jpg', f'images/validation/{image}.jpg')
-
-for image in test_images:
-    os.replace(f'images/{image}.jpg', f'images/test/{image}.jpg')
+for idx in df_test.index:
+    id = df_test.loc[idx, 'id']
+    label = df_test.loc[idx, 'Category']
+    os.replace(f'images/{id}.jpg', f'images/test/{label}/{id}.jpg')
 # %%
