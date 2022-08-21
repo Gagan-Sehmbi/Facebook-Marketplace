be38a938f0c9acaa1c756cc569e0186d3080d864
Gagan Sehmbi <gagan.sehmbi@outlook.com>
2022-08-21 15:01:26 +0100

    First Commit

---
+++ image_classification.py
@@ -0,0 +1,293 @@
+# %%
+# IMPORT LIBRARIES
+import pandas as pd
+import numpy as np
+import random
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torchvision
+from torchvision import transforms
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable
+
+import cv2
+from PIL import Image
+
+import glob
+
+# %%
+# CREATE TRAIN, VAL, TEST SETS
+
+train_data_path = 'images/train' 
+test_data_path = 'images/test'
+
+train_image_paths = [] #to store image paths in list
+classes = [] #to store class values
+
+for data_path in glob.glob(train_data_path + '/*'):
+    classes.append(data_path.split('/')[-1])
+    train_image_paths.append(glob.glob(data_path + '/*'))
+    
+train_image_paths = [item for sublist in train_image_paths for item in sublist]
+random.shuffle(train_image_paths)
+
+print('train_image_path example: ', train_image_paths[0])
+print('class example: ', classes[0])
+
+
+#split train valid from train paths (80,20)
+train_image_paths, valid_image_paths = train_image_paths[:int(0.75*len(train_image_paths))], train_image_paths[int(0.75*len(train_image_paths)):] 
+
+
+test_image_paths = []
+for data_path in glob.glob(test_data_path + '/*'):
+    test_image_paths.append(glob.glob(data_path + '/*'))
+
+test_image_paths = [item for sublist in test_image_paths for item in sublist]
+
+print("\nTrain size: {}\nValid size: {}\nTest size: {}".format(len(train_image_paths), len(valid_image_paths), len(test_image_paths)))
+
+# %% 
+# CREATE INDEX_TO_CLASS AND CLASS_TO_INDEX
+
+idx_to_class = {i:j for i, j in enumerate(classes)}
+class_to_idx = {value:key for key,value in idx_to_class.items()}
+idx_to_class
+
+# %%
+# CREATE TRANSFORM FUNCTION
+
+def transform(img, final_dim=128):
+    img_size = img.size
+    max_dim = max(img.size)
+    sf = final_dim/max_dim
+
+    new_img_size = (int(img_size[0]*sf), int(img_size[1]*sf))
+    new_img = img.resize(new_img_size)
+
+    final_img = Image.new(mode='RGB', size=(final_dim, final_dim))
+    final_img.paste(new_img, ((final_dim-new_img_size[0])//2, (final_dim-new_img_size[1])//2))
+
+    output = transforms.ToTensor()(final_img)
+    return output
+
+
+# %%
+# CREATE DATASET
+
+class FBMDataset(Dataset):
+    def __init__(self, image_paths, transform=False):
+        self.image_paths = image_paths
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = Image.open(image_filepath)
+        
+        label = image_filepath.split('/')[-2]
+        label = class_to_idx[label]
+        if self.transform is not None:
+            image = self.transform(img=image)
+        
+        return image, label
+
+train_dataset = FBMDataset(train_image_paths,transform=transform)
+valid_dataset = FBMDataset(valid_image_paths,transform=transform)
+test_dataset = FBMDataset(test_image_paths,transform=transform)
+
+print('The shape of tensor for 50th image in train dataset: ',train_dataset[49][0].shape)
+print('The label for 50th image in train dataset: ',train_dataset[49][1])
+
+batch_size = 64
+
+dataloader_train = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True
+)
+
+dataloader_val = DataLoader(
+    valid_dataset, batch_size=batch_size, shuffle=True
+)
+
+dataloader_test = DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False
+)
+
+print(next(iter(dataloader_train))[0].shape)
+print(next(iter(dataloader_train))[1].shape)
+
+# %%
+# BUILD MODEL
+
+# Set up CNN model
+
+# Define a convolution neural network
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(12)
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(12)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv4 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(24)
+        self.conv5 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=5, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(24)
+        self.fc1 = nn.Linear(80736, len(classes))
+
+    def forward(self, input):
+        output = F.relu(self.bn1(self.conv1(input)))      
+        output = F.relu(self.bn2(self.conv2(output)))     
+        output = self.pool(output)                        
+        output = F.relu(self.bn4(self.conv4(output)))     
+        output = F.relu(self.bn5(self.conv5(output)))     
+        output = output.view(-1, 80736)
+        output = self.fc1(output)
+
+        return output
+
+# Instantiate a neural network model 
+model = Network()
+
+
+# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+
+# Function to save the model
+def saveModel():
+    path = "./myFirstFBMModel.pth"
+    torch.save(model.state_dict(), path)
+
+# %%
+# TRAIN AND VALIDATE MODEL
+
+# Function to test the model with the test dataset and print the accuracy for the test images
+def testAccuracy():
+    
+    model.eval()
+    accuracy = 0.0
+    total = 0.0
+    
+    with torch.no_grad():
+        for data in dataloader_val:
+            images, labels = data
+            # run the model on the test set to predict labels
+            outputs = model(images)
+            # the label with the highest energy will be our prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
+    
+    # compute the accuracy over all test images
+    accuracy = (100 * accuracy / total)
+    return(accuracy)
+
+# Training function. We simply have to loop over our data iterator and feed the inputs to the network and optimize.
+def train(num_epochs):
+    
+    best_accuracy = 0.0
+
+    # Define your execution device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("The model will be running on", device, "device")
+    # Convert model parameters and buffers to CPU or Cuda
+    model.to(device)
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        running_acc = 0.0
+
+        for i, (images, labels) in enumerate(dataloader_train, 0):
+            
+            # get the inputs
+            images = Variable(images.to(device))
+            labels = Variable(labels.to(device))
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # predict classes using images from the training set
+            outputs = model(images)
+            # compute the loss based on model output and real labels
+            loss = loss_fn(outputs, labels)
+            # backpropagate the loss
+            loss.backward()
+            # adjust parameters based on the calculated gradients
+            optimizer.step()
+
+            # Let's print statistics for every 1,000 images
+            running_loss += loss.item()     # extract the loss value
+            if i % 1000 == 999:    
+                # print every 1000 (twice per epoch) 
+                print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 1000))
+                # zero the loss
+                running_loss = 0.0
+
+        # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
+        accuracy = testAccuracy()
+        print('For epoch', epoch+1,'the test accuracy over the whole test set is %d %%' % (accuracy))
+        
+        # we want to save the model if the accuracy is the best
+        if accuracy > best_accuracy:
+            saveModel()
+            best_accuracy = accuracy
+
+
+# Function to show the images
+def imageshow(img):
+    #img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# Function to test the model with a batch of images and show the labels predictions
+def testBatch():
+    # get batch of images from the test DataLoader  
+    images, labels = next(iter(dataloader_val))
+
+    # show all images as one image grid
+    imageshow(torchvision.utils.make_grid(images))
+   
+    # Show the real labels on the screen 
+    print('Real labels: ', ' '.join('%5s' % classes[labels[j]] for j in range(64)))
+  
+    # Let's see what if the model identifiers the  labels of those example
+    outputs = model(images)
+    
+    # We got the probability for every 10 labels. The highest (max) probability should be correct label
+    _, predicted = torch.max(outputs, 1)
+    
+    # Let's show the predicted labels on the screen to compare with the real ones
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(batch_size)))
+
+
+#%%
+if __name__ == "__main__":
+    
+    # Let's build our model
+    train(5)
+    print('Finished Training')
+
+    # Test which classes performed well
+    testModelAccuracy()
+    
+    # Let's load the model we just created and test the accuracy per label
+    model = Network()
+    path = "myFirstModel.pth"
+    model.load_state_dict(torch.load(path))
+
+    # Test with batch of images
+    testBatch()
+# %%
---
+++ text_classification.py
@@ -0,0 +1,167 @@
+#%%
+# IMPORT LIBRARIES
+
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sklearn
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, f1_score
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+import transformers
+from transformers import DistilBertTokenizer, DistilBertModel
+
+#%%
+# IMPORT DATA
+
+df = pd.read_csv('tabular_data_train.csv')
+
+encoder = LabelEncoder()
+
+df['label'] = encoder.fit_transform(df['Category'])
+df['label'] = df['label'].astype('int64')
+num_classes = max(df['label'])+1
+
+# %%
+# CREATE CUSTOM DATASET
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+
+class DataPrep(Dataset):
+    def __init__(self, df, tokenizer, max_len):
+        self.data = df
+        self.len = len(df)
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __getitem__(self, index):
+        text = str(self.data['Description'][index])
+        embeddings = self.tokenizer(text,
+                                    add_special_tokens = True,
+                                    max_length = self.max_len,
+                                    truncation = True,
+                                    padding = 'max_length')
+        
+        return {
+            'idx': torch.tensor(index),
+            'ids': torch.tensor(embeddings['input_ids']),
+            'mask': torch.tensor(embeddings['attention_mask']),
+            'target': torch.tensor(self.data['label'][index])
+        }
+            
+            
+    def __len__(self):
+            return self.len
+                                    
+                                    
+df_train, df_val = train_test_split(df, test_size=0.25, shuffle=True)
+df_train.reset_index(drop=True, inplace=True)
+df_val.reset_index(drop=True, inplace=True)
+
+dataset_train = DataPrep(df_train, tokenizer, 256)
+dataset_val = DataPrep(df_train, tokenizer, 256)
+
+dataloader_train = DataLoader(dataset_train, batch_size=8, shuffle=True)
+dataloader_val = DataLoader(dataset_val, batch_size=8, shuffle=True)
+
+
+#%%
+# MODEL
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+class FBMClassifier(torch.nn.Module):
+    def __init__(self):
+        super(FBMClassifier, self).__init__()
+        self.l1 = DistilBertModel.from_pretrained('distilbert-base-cased')
+        self.l2 = torch.nn.Linear(768,768)
+        self.l3 = torch.nn.Dropout(0.3)
+        self.l4 = torch.nn.Linear(768, num_classes)
+        
+    def forward(self, ids, mask):
+        out = self.l1(ids, mask)
+        out = out[0][:,0]
+        out = self.l2(out)
+        out = torch.nn.ReLU()(out)
+        out = self.l3(out)
+        out = self.l4(out)
+        
+        return out
+    
+    def step(self, batch):
+        ids = batch['ids'].to(device)
+        mask = batch['mask'].to(device)
+        target = batch['target'].to(device)
+        
+        output = self(ids, mask)
+        
+        loss = loss_func(output, target)
+        
+        return output, target, loss
+
+
+model = FBMClassifier()
+model.to(device)
+loss_func = torch.nn.CrossEntropyLoss()
+opt_func = torch.optim.Adam
+
+
+def evaluate(model, val_loader, epoch):
+    guess = []
+    real = []
+    with torch.no_grad():
+        for batch in val_loader:
+            output, target, loss = model.step(batch)
+            _, pred = torch.max(output.data, dim=1)
+            guess.append(pred.cpu().detach().tolist())
+            real.append(target.cpu().detach().tolist())
+        r = [j for i in real for j in i]
+        g = [j for i in guess for j in i]
+        cr = classification_report(r, g)
+        print(f'Epoch{epoch+1}: Validation Performance\n', cr)
+        validation_score = f1_score(r, g, average=None)
+        return validation_score
+
+def fit(epochs, lr, model, train_loader, val_loader):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    
+    for epoch in range(epochs):
+        guess = []
+        real = []
+        for batch in train_loader:
+            output, target, loss = model.step(batch)
+            _, pred = torch.max(output.data, dim=1)
+            guess.append(pred.cpu().detach().tolist())
+            real.append(target.cpu().detach().tolist())
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        r = [j for i in real for j in i]
+        g = [j for i in guess for j in i]
+        cr = classification_report(r, g)
+        print(f'Epoch{epoch+1}: Training Performance\n', cr)
+        training_score = f1_score(r, g, average=None)
+        
+        validation_score = evaluate(model, val_loader, epoch)
+        
+    return training_score, validation_score
+
+
+training_score, validation_score = fit(epochs=5,
+                                       lr=1e-5, 
+                                       model=model, 
+                                       train_loader=dataloader_train,
+                                       val_loader=dataloader_val)
+
+
+# %%
